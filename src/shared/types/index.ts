52ab83f641e3c9e8e5c4b050a746176598832264
lena <lena.rashkovan@free-now.com>
2023-09-20 20:22:12 +0200

    feat: add entity types

---
+++ src/shared/types/index.ts
@@ -0,0 +1,7 @@
+export * from './amenity-type.enum.js';
+export * from './city.enum.js';
+export * from './comment.type.js';
+export * from './coordinate.type.js';
+export * from './city.enum.js';
+export * from './city.enum.js';
+export * from './city.enum.js';
---
+++ src/shared/types/offer.type.ts
@@ -0,0 +1,25 @@
+import { City } from './city.enum.js';
+import { HousingType } from './housing-type.enum.js';
+import { AmenityType } from './amenity-type.enum.js';
+import { User } from './user.type.js';
+import { Coordinates } from './coordinate.type.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  city: City;
+  imagePreview: string;
+  images: string[];
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  housingType: HousingType;
+  roomAmount: number;
+  guestAmount: number;
+  price: number;
+  amenities: AmenityType[];
+  author: User;
+  commentAmount: number;
+  location: Coordinates;
+};
