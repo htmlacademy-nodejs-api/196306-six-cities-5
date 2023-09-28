98fec9918c8116fada3842896e6ee461a78a4297
lena <lena.rashkovan@free-now.com>
2023-09-28 23:01:32 +0200

    feat(mocks): add mock-server-data

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,6 +3,7 @@ export * from './city.enum.js';
 export * from './comment.type.js';
 export * from './coordinate.type.js';
 export * from './housing-type.enum.js';
+export * from './mock-server-data.type.js';
 export * from './offer.type.js';
 export * from './user.type.js';
 export * from './user-type.enum.js';
---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,9 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  previewImages: string[];
+  roomImages: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+};
