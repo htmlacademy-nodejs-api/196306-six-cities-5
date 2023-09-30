aff330efc75a8633f24a6e1ca98fe65eb831721a
lena <lena.rashkovan@free-now.com>
2023-09-30 14:03:31 +0200

    style(tsv-file-reader): remove initialization as part of expression
    D13

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -21,10 +21,12 @@ export class TSVFileReader extends EventEmitter implements FileReader {
 
     readStream.on('data', (chunk) => {
       remainingData += chunk.toString();
+      nextLinePosition = remainingData.indexOf('\n');
 
-      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+      while (nextLinePosition >= 0) {
         const completeRow = remainingData.slice(0, nextLinePosition + 1);
         remainingData = remainingData.slice(++nextLinePosition);
+        nextLinePosition = remainingData.indexOf('\n');
         importedRowCount++;
 
         this.emit('line', completeRow);
