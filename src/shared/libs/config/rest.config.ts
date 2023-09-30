9f5401e65037e6209fd3af5579b5298b353d0ade
lena <lena.rashkovan@free-now.com>
2023-09-30 13:46:23 +0200

    feat(rest): inject dependencies

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { Config } from './config.interface.js';
 import { Logger } from '../logger/index.js';
 import { configRestSchema, RestSchema } from './rest.schema.js';
