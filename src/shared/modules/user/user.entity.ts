75778007d2b476d69ab32b3e42a8bc5c0c02afb0
lena <lena.rashkovan@free-now.com>
2023-10-03 19:12:44 +0200

    feat(user): add methods to handle passwords

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -4,7 +4,9 @@ import {
   defaultClasses,
   modelOptions,
 } from '@typegoose/typegoose';
+
 import { User, UserType } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
 export interface UserEntity extends defaultClasses.Base {}
 
@@ -21,14 +23,35 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: false, default: '' })
   public avatarPath = '';
 
-  @prop({ required: true })
+  @prop({ required: true, default: '' })
   public name = '';
 
-  @prop({ required: true })
-  public password = '';
+  @prop({ required: true, default: '' })
+  private password?: string;
 
-  @prop({ required: true })
-  public type = UserType.Normal;
+  @prop({
+    type: () => String,
+    enum: UserType,
+    required: true,
+  })
+  public type: UserType;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.avatarPath = userData.avatarPath;
+    this.name = userData.name;
+    this.type = userData.type;
+  }
+
+  public getPassword() {
+    return this.password;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
