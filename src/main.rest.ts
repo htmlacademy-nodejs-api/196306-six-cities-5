282a0c82ab250e57530cbda4a67b6e51c99aa0d1
lena <lena.rashkovan@free-now.com>
2023-10-09 17:39:23 +0200

    feat(main): use offer container

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,9 +3,10 @@ import { Container } from 'inversify';
 import { Component } from './shared/types/index.js';
 import { RestApplication, createRestApplicationContainer } from './rest/index.js';
 import { createUserContainer } from './shared/modules/user/index.js';
+import { createOfferContainer } from './shared/modules/offer/index.js';
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer());
+  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer(), createOfferContainer());
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
