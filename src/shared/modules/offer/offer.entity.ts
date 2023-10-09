f6f893c4b1df411eb6dbf79f9b46ad30e2dc2c27
lena <lena.rashkovan@free-now.com>
2023-10-09 17:30:26 +0200

    feat(offer): add OfferEntity

---
+++ src/shared/modules/offer/offer.entity.ts
@@ -0,0 +1,81 @@
+import { defaultClasses, getModelForClass, modelOptions, prop, Ref } from '@typegoose/typegoose';
+import { AmenityType, City, Coordinates, HousingType } from '../../types/index.js';
+import { UserEntity } from '../user/index.js';
+
+export interface OfferEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'offers',
+    timestamps: true,
+  },
+})
+export class OfferEntity extends defaultClasses.TimeStamps {
+  @prop({ trim: true, required: true })
+  public title: string;
+
+  @prop({ trim: true, required: true })
+  public description: string;
+
+  @prop({ required: true })
+  public postDate: Date;
+
+  @prop({
+    type: String,
+    enum: City,
+    required: true,
+  })
+  public city: City;
+
+  @prop({ required: true })
+  public imagePreview: string;
+
+  @prop({ required: true })
+  public images: string[];
+
+  @prop({ required: true })
+  public isPremium: boolean;
+
+  @prop({ required: true })
+  public isFavorite: boolean;
+
+  @prop({ required: true })
+  public rating: number;
+
+  @prop({
+    type: String,
+    enum: HousingType,
+    required: true,
+  })
+  public housingType: HousingType;
+
+  @prop({ required: true })
+  public roomAmount: number;
+
+  @prop({ required: true })
+  public guestAmount: number;
+
+  @prop({ required: true })
+  public price: number;
+
+  @prop({
+    type: String,
+    enum: AmenityType,
+    required: true,
+  })
+  public amenities: AmenityType[];
+
+  @prop({ default: 0 })
+  public commentAmount: number;
+
+  @prop({ required: true })
+  public location: Coordinates;
+
+  @prop({
+    ref: UserEntity,
+    required: true,
+  })
+  public authorId: Ref<UserEntity>;
+}
+
+export const OfferModel = getModelForClass(OfferEntity);
