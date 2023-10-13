5587f4bf964d00b9bb6ae298bda670dfa9facd19
lena <lena.rashkovan@free-now.com>
2023-10-13 17:38:37 +0200

    feat(user): add /login route

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -10,6 +10,7 @@ import { UserService } from './user-service.interface.js';
 import { CreateUserRequest } from './create-user-request.type.js';
 import { fillDTO } from '../../helpers/index.js';
 import { UserRdo } from './rdo/user.rdo.js';
+import { LoginUserRequest } from './login-user-request.type.js';
 
 @injectable()
 export class UserController extends BaseController {
@@ -23,20 +24,27 @@ export class UserController extends BaseController {
     this.logger.info('Registering routes for UserController…');
 
     this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: '/login', method: HttpMethod.Post, handler: this.login });
   }
 
   public async create({ body }: CreateUserRequest, res: Response): Promise<void> {
     const isExistingUser = await this.userService.findByEmail(body.email);
 
     if (isExistingUser) {
-      throw new HttpError(
-        StatusCodes.CONFLICT,
-        `User with email "${body.email}" already exists.`,
-        'UserController'
-      );
+      throw new HttpError(StatusCodes.CONFLICT, `User with email "${body.email}" already exists.`, 'UserController');
     }
 
     const result = await this.userService.create(body, this.configService.get('SALT'));
     this.created(res, fillDTO(UserRdo, result));
+  }
+
+  public async login({ body }: LoginUserRequest, _res: Response): Promise<void> {
+    const isExistingUser = await this.userService.findByEmail(body.email);
+
+    if (!isExistingUser) {
+      throw new HttpError(StatusCodes.UNAUTHORIZED, 'User with specified credentials was not found.', 'UserController');
+    }
+
+    throw new HttpError(StatusCodes.NOT_IMPLEMENTED, 'Not implemented', 'UserController');
   }
 }
