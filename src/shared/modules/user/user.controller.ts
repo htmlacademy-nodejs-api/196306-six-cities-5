7be18e71f6ca108ec02c65e89db37fc73ca7e286
lena <lena.rashkovan@free-now.com>
2023-10-13 17:18:45 +0200

    feat(user): add /register route

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,27 +1,42 @@
 import { inject, injectable } from 'inversify';
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
+import { HttpError } from '../../libs/rest/errors/index.js';
+import { Config, RestSchema } from '../../libs/config/index.js';
 import { Component } from '../../types/index.js';
+import { UserService } from './user-service.interface.js';
+import { CreateUserRequest } from './create-user-request.type.js';
+import { fillDTO } from '../../helpers/index.js';
+import { UserRdo } from './rdo/user.rdo.js';
 
 @injectable()
 export class UserController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.UserService) private readonly userService: UserService,
+    @inject(Component.Config) private readonly configService: Config<RestSchema>,
   ) {
     super(logger);
 
-    this.logger.info('Register routes for UserController…');
+    this.logger.info('Registering routes for UserController…');
 
-    this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
-    this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
   }
 
-  public index(_req: Request, _res: Response): void {
-    // Код обработчика
-  }
+  public async create({ body }: CreateUserRequest, res: Response): Promise<void> {
+    const isExistingUser = await this.userService.findByEmail(body.email);
 
-  public create(_req: Request, _res: Response): void {
-    // Код обработчика
+    if (isExistingUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email "${body.email}" already exists.`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.created(res, fillDTO(UserRdo, result));
   }
 }
