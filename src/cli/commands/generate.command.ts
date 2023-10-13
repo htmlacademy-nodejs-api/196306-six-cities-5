61d668ae37b219708cfc67148eb42f0faaa94298
lena <lena.rashkovan@free-now.com>
2023-10-13 16:27:46 +0200

    chore: clean up imports

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,7 +1,7 @@
 import got from 'got';
 import { Command } from './command.interface.js';
 import { MockServerData } from '../../shared/types/index.js';
-import { TSVOfferGenerator } from '../../shared/libs/index.js';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/index.js';
 import { TSVFileWriter } from '../../shared/libs/file-writer/index.js';
 import { ConsoleLogger, Logger } from '../../shared/libs/logger/index.js';
 
