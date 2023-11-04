76080c620a1bf28db7132c1f955b14e266cbe380
Lena Rashkovan <lena.rashkovan@free-now.com>
2023-11-04 17:26:45 +0100

    refactor(rest): fix review comments

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -12,7 +12,7 @@ import { STATIC_FILES_ROUTE, STATIC_UPLOAD_ROUTE } from './rest.constant.js';
 
 @injectable()
 export class RestApplication {
-  private server: Express;
+  private server: Express = express();
 
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
@@ -27,11 +27,9 @@ export class RestApplication {
     @inject(Component.HttpExceptionFilter) private readonly httpExceptionFilter: ExceptionFilter,
     @inject(Component.ValidationExceptionFilter) private readonly validationExceptionFilter: ExceptionFilter,
     @inject(Component.FileUploadExceptionFilter) private readonly fileUploadExceptionFilter: ExceptionFilter
-  ) {
-    this.server = express();
-  }
+  ) {}
 
-  private async _initDb() {
+  private async initDb() {
     const mongoUri = getMongoURI(
       this.config.get('DB_USER'),
       this.config.get('DB_PASSWORD'),
@@ -46,19 +44,19 @@ export class RestApplication {
     });
   }
 
-  private async _initServer() {
+  private async initServer() {
     const port = this.config.get('PORT');
     this.server.listen(port);
   }
 
-  private async _initControllers() {
+  private async initControllers() {
     this.server.use('/users', this.userController.router);
     this.server.use('/offers', this.offerController.router);
     this.server.use('/comments', this.commentController.router);
     this.server.use('/cities', this.cityController.router);
   }
 
-  private async _initMiddleware() {
+  private async initMiddleware() {
     const authenticateMiddleware = new ParseTokenMiddleware(
       this.config.get('JWT_SECRET')
     );
@@ -70,7 +68,7 @@ export class RestApplication {
     this.server.use(cors());
   }
 
-  private async _initExceptionFilters() {
+  private async initExceptionFilters() {
     this.server.use(this.authExceptionFilter.catch.bind(this.authExceptionFilter));
     this.server.use(this.validationExceptionFilter.catch.bind(this.validationExceptionFilter));
     this.server.use(this.fileUploadExceptionFilter.catch.bind(this.fileUploadExceptionFilter));
@@ -82,23 +80,23 @@ export class RestApplication {
     this.logger.info('Application initialization');
 
     this.logger.info('Initializing database…');
-    await this._initDb();
+    await this.initDb();
     this.logger.info('Database initialization complete.');
 
     this.logger.info('Initializing app-level middleware…');
-    await this._initMiddleware();
+    await this.initMiddleware();
     this.logger.info('App-level middleware initialization complete.');
 
     this.logger.info('Initializing controllers…');
-    await this._initControllers();
+    await this.initControllers();
     this.logger.info('Controller initialization complete.');
 
     this.logger.info('Initializing exception filters…');
-    await this._initExceptionFilters();
+    await this.initExceptionFilters();
     this.logger.info('Exception filters initialization complete.');
 
     this.logger.info('Initializing server…');
-    await this._initServer();
+    await this.initServer();
     this.logger.info(`🚀 Server started on ${getFullServerPath(this.config.get('HOST'), this.config.get('PORT'))}`);
   }
 }
