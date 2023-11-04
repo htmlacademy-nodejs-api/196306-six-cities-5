0fd5a80c90d9eb7aaf8821661e5bfa35a29321fc
Lena Rashkovan <lena.rashkovan@free-now.com>
2023-11-04 19:25:23 +0100

    refactor(user): use constants

--- src/shared/modules/user/user.constant.ts
+++ src/shared/modules/user/user.constant.ts
@@ -1,1 +1,11 @@
 export const DEFAULT_AVATAR_FILE_NAME = 'default-avatar.svg';
+
+export const NAME_LENGTH = {
+  MIN: 1,
+  MAX: 15
+};
+
+export const PASSWORD_LENGTH = {
+  MIN: 6,
+  MAX: 12
+};
